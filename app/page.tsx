ecc86048dcd9c40825804622a3326a4072478c13
ChipRealz <21110247@student.hcmute.edu.vn>
2024-09-17 15:24:58 +0700

    modify page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div>
       <h1 className="h1-bold">I am comming home</h1>
       <h2 className="h2-bold">I am comming home</h2>
+      <h3 className="h3-bold">I am free</h3>
     </div>
   );
 }
