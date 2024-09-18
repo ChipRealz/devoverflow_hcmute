6f788424e30fb611d018dd06ac80e718c4759434
ChipRealz <21110247@student.hcmute.edu.vn>
2024-09-18 17:32:38 +0700

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,44 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+import { Inter, Space_Grotesk } from 'next/font/google';
+import type {Metadata} from 'next'
+import React from 'react'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight:['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight:['300', '400', '500', '600', '700'],
+})
+
+export const metadata: Metadata = {
+  title: 'DevOverFlow',
+  description: 'A place for developers to ask questions and find answers',
+  icons: {
+    icon:'/assets/images/site-logo.svg',
+  }
+}
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: 'primary-text-gradient hover:text-primary-500',
+      }
+    }}
+    >
       <html lang="en">
-        <body>
-          {/* <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn> */}
+        <body className={`${inter.className} ${spaceGrotesk.className}`}>
           {children}
         </body>
       </html>
