709ff712d56e7e2417b5ea120a69f0c62d2071af
ChipRealz <21110247@student.hcmute.edu.vn>
2024-09-18 16:51:55 +0700

    auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">I am comming home</h1>
-      <h2 className="h2-bold">I am comming home</h2>
-      <h3 className="h3-bold">I am free</h3>
-    </div>
-  );
-}
