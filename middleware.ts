03bb2df82815d29de99e72d2baaaf7933b0a1962
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-07 23:47:38 +0700

    test

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks(.*)']);
+const isProtectedRoute = createRouteMatcher([ '/api/webhooks(.*)']);
 
 export default clerkMiddleware((auth, request) => {
-  if (isPublicRoute(request)) auth().protect();
+  if (isProtectedRoute(request)) auth().protect();
 });
 
 export const config = {
