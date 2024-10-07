94f1cd1f281990d3d875740d21ded1980a8ca527
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-07 23:16:48 +0700

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
-
-export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Skip Next.js internals and all static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Match all API routes except /api/webhook and /api/chatgpt
+    '/api((?!/webhook|/chatgpt).*)',  // This matches all API routes except specified ones
+    // Public routes that should not trigger the middleware
+    '/',
+    '/question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs',
+    // Add more public routes if necessary
   ],
 };
