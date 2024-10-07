e6a206170a1594a817e0e5d6e416749b7411b598
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-07 22:49:09 +0700

    testnlan

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/api/webhook",
-    "/question/:id",
-    "/tags",
-    "/tags/:id",
-    "/profile/:id",
-    "/community",
-    "/jobs",
-    "/sign-up",
-    "/sign-in",
-  ],
-  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
+const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
