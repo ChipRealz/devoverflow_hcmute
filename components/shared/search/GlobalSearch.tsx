f9bb1e20056fbbc7a8620434c3b0f527aad405db
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-07 21:26:09 +0700

    test

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input"
 const GlobalSearch = () => {
   return (
     <div className='relative w-full max-w-[600px] max-lg:hidden'>
-        <div className='background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-x1 px-4'>
+        <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
             <Image
                 src="/assets/icons/search.svg"
                 width={24}
