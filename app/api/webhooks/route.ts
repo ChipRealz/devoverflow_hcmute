90b253dced3b30993e10ba783904dd63506aeb6c
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-08 20:04:20 +0700

    test webhook again

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -8,7 +8,8 @@ import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
  
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+
  
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
@@ -66,10 +67,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ message: 'OK', user: mongoUser})
   }
-  
-  if (evt.type === 'user.created') {
-    console.log('userId:', evt.data.id)
-  }
+
   
   if(eventType === 'user.updated') {
     const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
