fafbbf96594f7ccd8865c3b6a7d31475f05617eb
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-15 17:16:58 +0700

    done community search

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -92,9 +92,17 @@ export async function getAllUsers(params: GetAllUsersParams) {
   try {
     connectToDatabase();
 
-    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+    const { searchQuery } = params;
 
-    const users = await User.find({})
+    const query: FilterQuery<typeof User> = {};
+    if(searchQuery){
+      query.$or = [
+        { name: { $regex: new RegExp(searchQuery, 'i') }},
+        { username: { $regex: new RegExp(searchQuery, 'i') }},
+      ]
+    }
+
+    const users = await User.find(query)
       .sort({ createdAt: -1 })
 
     return { users };
