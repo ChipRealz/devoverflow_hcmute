22cd98aa6b98d566ca4a61b6f0113c379e3165b8
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-15 17:40:58 +0700

    collection search done

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -153,9 +153,15 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
 
     const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
     
-    const query: FilterQuery<typeof Question> = searchQuery
-      ? { title: { $regex: new RegExp(searchQuery, 'i') } }
-      : { };
+    const query: FilterQuery<typeof Question> = {};
+    if(searchQuery){
+      query.$or =[
+        { title: { $regex: new RegExp(searchQuery, 'i') }},
+      ]
+    }
+      
+    // ? { title: { $regex: new RegExp(searchQuery, 'i') } }
+      // : {};
 
     const user = await User.findOne({ clerkId }).populate({
       path: 'saved',
