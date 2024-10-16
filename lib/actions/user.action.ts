0948e776a47c392c050f18d14138384903ea3f0b
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-16 22:01:45 +0700

    questiontab pagination

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -269,16 +269,22 @@ export async function getUserQuestions(params: GetUserStatsParams) {
   try {
     connectToDatabase();
 
-    const { userId, page = 1, pageSize = 10 } = params;
+    const { userId, page = 1, pageSize = 5 } = params;
+
+    const skipAmount = (page - 1) * pageSize;
 
     const totalQuestions = await Question.countDocuments({ author: userId})
 
     const userQuestions = await Question.find({ author: userId })
       .sort({ views: -1, upvotes: -1 })
+      .skip(skipAmount)
+      .limit(pageSize)
       .populate('tags', '_id name')
       .populate('author', '_id clerkId name picture')
 
-    return { totalQuestions, questions: userQuestions };
+      const isNextQuestions = totalQuestions > skipAmount + userQuestions.length;
+    
+    return { totalQuestions, questions: userQuestions, isNextQuestions };
   } catch (error) {
     console.log(error);
     throw error;
