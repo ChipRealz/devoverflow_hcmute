382da0940b94a6ce5a054d9ccb686cee926c118e
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-16 18:01:11 +0700

    collection pagination

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -177,7 +177,9 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { clerkId, searchQuery, filter } = params;
+    const { clerkId, searchQuery, filter, page = 1, pageSize = 10 } = params;
+
+    const skipAmount = (page - 1) * pageSize;
     
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, 'i') } }
@@ -206,11 +208,15 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
           break;
       }
 
-    const user = await User.findOne({ clerkId }).populate({
+    const user = await User
+    .findOne({ clerkId })
+    .populate({
       path: 'saved',
       match: query,
       options: {
         sort: sortOptions,
+        skip: skipAmount,
+        limit: pageSize + 1,
       },
       populate: [
         { path: 'tags', model: Tag, select: "_id name" },
@@ -218,13 +224,15 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
       ]
     })
 
+    const isNext = user.saved.length > pageSize;
+    
     if(!user) {
       throw new Error('User not found');
     }
 
     const savedQuestions = user.saved;
 
-    return { questions: savedQuestions };
+    return { questions: savedQuestions, isNext };
   } catch (error) {
     console.log(error);
     throw error;
