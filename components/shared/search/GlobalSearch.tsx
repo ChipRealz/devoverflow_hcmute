d37e110a5f1b90217a516c912a4594af2a903e88
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-17 23:13:15 +0700

    need to fix

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,15 +1,47 @@
 "use client"
 
 import Image from 'next/image'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from "@/components/ui/input"
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils'
+import GlobalResult from './GlobalResult'
 
 
 const GlobalSearch = () => {
 
-  useEffect(() => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  }, []);
+  const query = searchParams.get('q');
+
+  const [search, setSearch] = useState(query || '');
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if(search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key:'global',
+          value: search
+        })
+
+        router.push( newUrl, { scroll: false});
+      } else {
+        if (query){
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ['global', 'type'],
+          })
+          router.push( newUrl, { scroll: false});
+        }
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, pathname, router, searchParams, query]);
 
   return (
     <div className='relative w-full max-w-[600px] max-lg:hidden'>
@@ -24,10 +56,22 @@ const GlobalSearch = () => {
 
             <Input
             type='text'
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+
+              if (!isOpen) {
+                setIsOpen(true);
+              }
+              if (e.target.value === '' && isOpen) {
+                setIsOpen(false);
+              }
+            }}
             placeholder='Search...'
-            className='paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none'
+            className='paragraph-regular no-focus placeholder background-light800_darkgradient text-dark400_light700 border-none shadow-none outline-none'
             />
         </div>
+        {isOpen && <GlobalResult/>}
     </div>
   )
 }
