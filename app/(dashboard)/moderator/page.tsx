2e7725ec59d00b501118e4e73cb8f1a7b4af6d31
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-26 00:17:10 +0700

    moderator homepage

--- app/(dashboard)/moderator/page.tsx
+++ app/(dashboard)/moderator/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Card, CardContent, CardHeader, CardDescription } from "@/components/ui/card";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
 
 const Page = () => {
   return (
-    <div>Moderator Page</div>
-  )
-}
+    <>
+      <div className="mt-4">
+        <Card>
+          <CardHeader>
+            <CardDescription>
+              Home of the moderators!
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>
+              Welcome to the moderator dashboard. Here you can manage content, review reports, and perform other moderation tasks.
+            </p>
+          </CardContent>
+        </Card>
+      </div>
 
-export default Page
+      <div className="mt-4">
+        <Alert>
+          <Terminal className="size-4" />
+          <AlertTitle>Heads up!</AlertTitle>
+          <AlertDescription>
+            Keep up the great work, moderators! Your efforts make our community a better place every day!
+          </AlertDescription>
+        </Alert>
+      </div>
+    </>
+  );
+};
+
+export default Page;
