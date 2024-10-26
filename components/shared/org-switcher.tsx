7fffc3f0d4ac01823d6f4cf9651c019286e8f305
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-26 20:28:36 +0700

    commit

--- components/shared/org-switcher.tsx
+++ components/shared/org-switcher.tsx
@@ -3,7 +3,7 @@ import { OrganizationSwitcher } from "@clerk/nextjs"
 export const OrgSwitcher = () => {
     return (
         <div className="flex">
-            <OrganizationSwitcher afterSelectOrganizationUrl="/moderator" defaultOpen />
+            <OrganizationSwitcher afterSelectOrganizationUrl="/moderator" defaultOpen={false} />
         </div>
     )
 }
