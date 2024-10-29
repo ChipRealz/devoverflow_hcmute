4556f7ab7c7142a80b8f6f593b46cb46d3e0fd9a
ChipRealz <21110247@student.hcmute.edu.vn>
2024-10-29 22:37:37 +0700

    Finish the management part for questions

--- app/(dashboard)/dashboard/moderator/questions/page.tsx
+++ app/(dashboard)/dashboard/moderator/questions/page.tsx
@@ -1,8 +1,62 @@
+import ManagerQuestionCard from '@/components/cards/ManagerQuestionCard';
+import NoResult from '@/components/shared/NoResult';
+import Pagination from '@/components/shared/Pagination';
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import { getQuestions } from '@/lib/actions/question.action';
+import { SearchParamsProps } from '@/types'
 import React from 'react'
 
-const page = () => {
+const page = async ({searchParams}: SearchParamsProps) => {
+  const result = await getQuestions({
+    searchQuery: searchParams.q,
+    filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
+  }); 
+
   return (
-    <div>questions</div>
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">Manage All Questions</h1> 
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+      <LocalSearchbar 
+          route="/dashboard/moderator/questions"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+      </div>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ?
+          result.questions.map((question) => (
+            <ManagerQuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+          : <NoResult 
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />}
+      </div>
+      <div className="mt-9">
+      <Pagination
+        pageNumber = {searchParams?.page ? +searchParams.page : 1}
+        isNext = {result.isNext}
+      />
+      </div>
+    </>
   )
 }
 
