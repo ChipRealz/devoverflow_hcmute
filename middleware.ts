45d7b9e57560c51fb5447bad0df499d78bcb6829
ChipRealz <21110247@student.hcmute.edu.vn>
2024-11-11 22:38:11 +0700

    test when upgrade to next 15

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/ask-question(.*)","/jobs"]);
+const isProtectedRoute = createRouteMatcher(["/ask-question(.*)", "/jobs"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) (await auth()).redirectToSignIn();
+  if (isProtectedRoute(req)) await auth();
 });
 
 export const config = {
