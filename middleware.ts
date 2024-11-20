f9e789e84b5aa85c89f864d807e4d288d6f6e1fd
ChipRealz <21110247@student.hcmute.edu.vn>
2024-11-20 20:02:58 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/ask-question(.*)","/jobs","/collection"]);
+const isProtectedRoute = createRouteMatcher(["/ask-question(.*)","/jobs","/collection","/profile"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
