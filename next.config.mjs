182632fb83540fd70fb47baff4272cedf4e45294
ChipRealz <21110247@student.hcmute.edu.vn>
2025-06-21 14:33:33 +0700

    Update next.config.mjs to use remotePatterns for image configuration instead of domains for improved flexibility in handling image sources.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    domains: ['img.clerk.com'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
   },
 };
 
